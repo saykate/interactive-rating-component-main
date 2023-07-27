d739544ae757deae547022b2008d5d42e73975fb
Katie Sayers <kate@student.dev-island.org>
2023-07-26 16:16:35 -1000

    finsihed up the js and got the functions sorted out

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ let submit = document.querySelector('.submit')
 let ratingsPage = document.querySelector('.rating-page')
 let thanksPage = document.querySelector('.thanks-page')
 
-ratings.forEach(rating => {
-    rating.addEventListener('click', () => {
-        yourRating.innerText = `You selected ${rating.textContent} out of 5`;
-        rating.classList.add('active')
+function getRating(e) {
+    ratings.forEach(rating => {
+        rating.classList.remove('active')
     })
-})
+    e.target.classList.add('active');
+    yourRating.innerText = `You selected ${e.target.textContent} out of 5`;
+}
 
 const submitRating = () => {
     thanksPage.classList.remove('hidden');
     ratingsPage.classList.add('hidden');
 }
+
+
+ratings.forEach(rating => {
+    rating.addEventListener('click', getRating);
+})
 
 submit.addEventListener('click', submitRating)
 
